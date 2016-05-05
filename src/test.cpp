60cc7bd73b1b917822dc96d2703232141c5e03df
Tamas Dozsa <tamasdzs@gmail.com>
2016-05-05 16:18:15 +0200

    added main() function (test.cpp)

---
+++ src/test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <typeinfo>
+#include <string>
+#include <vector>
+#include "Hermite.h"
+#include "EcgSigPrep.h"
+#include "OrtCompresser.h"
+
+int main () {
+	OrtCompresser OC("103", 2, 900);
+	Hermite H(OC.getSignal()->cols());
+	Eigen::MatrixXd Hsys = H.get_ort_fun_sys()->block(0, 0, OC.getSignal()->cols(), 20);
+	
+     
+     
+     
+    OrtCompressed result;
+    
+    std::vector<int> rounds;
+    rounds.push_back(7);
+    rounds.push_back(10);
+    rounds.push_back(17);
+    
+    result = OC.compressBeat(rounds); 
+     
+	const Eigen::MatrixXd res = OC.decompress(result);
+	std::cout<<res.transpose()<<std::endl;
+	
+	return 0;
+}
