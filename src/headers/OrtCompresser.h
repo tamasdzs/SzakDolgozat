775522b5c7b6bbba7a1eee14678b079535784f46
Tamas Dozsa <tamasdzs@gmail.com>
2016-05-07 17:37:31 +0200

    Added comments (possible solution plan) for OrtCompresser.h + modified UML

--- src/headers/OrtCompresser.h
+++ src/headers/OrtCompresser.h
@@ -3,13 +3,29 @@
 
 #include "Compresser.h"
 #include "Hermite.h"
-//#include "NelderMead.h"
+
+/* 
+ * CLASS: OrtCompresser
+ * Child class of compresser, the puropose of this class is to compress
+ * RECORD using some orthonormal polynomial based approach. 
+ * 
+ * TODO:
+ * 		-ERROR: The compression algorithm uses optimization to calculate the
+ * 				best possible dilatation and translation parameters for the heartbeat.
+ * 				The problem is, that the optimization algorithms themselves need to compress
+ * 				and decompress the heartbeat, and this means that while the optimizer 
+ * 				class is part of OrtCompresser, OrtCompresser needs also be part of
+ * 				the optimizer -> include CONFLICT
+ * 				
+ * 				POSSIBLE SOLUTION:  Create inbetween class, that is responsible for 
+ * 									compression with optimizitation. This class can 
+ * 									contain as members both an instance of the OrtCompresser	
+ * 									class AND and instance of the optimizer class
+ */
 
 class OrtCompresser: public Compresser {
 	protected:
 		OrtFunSys* ort_sys;
-		
-		
 		Eigen::MatrixXd calcAprx(const Eigen::MatrixXd& Coeffs, const Eigen::MatrixXd& H);
 		
 	public:
@@ -19,7 +35,6 @@ class OrtCompresser: public Compresser {
 		Eigen::MatrixXd calcFourier(const Eigen::MatrixXd& H, Eigen::MatrixXd& sig, const double lambda, const double trans);
 		
 		OrtCompressed compressBeat(const std::vector<int>& rounds);
-		//const OrtCompressed compressRec(std::vector<int>& rounds);
 		const Eigen::MatrixXd decompress(const OrtCompressed compr);
 		const double getPRD(const Eigen::MatrixXd& APR); 
 		const double getErr(const Eigen::MatrixXd& Coeffs);
